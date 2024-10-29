ab887d46c1d981986fd67f0a7de0fd27dd86351d
Itron_Al_Lenn <itronallenn@gmail.com>
2024-10-29 15:14:00 +0100

    Changed how lazynvim is loaded

--- init.lua
+++ init.lua
@@ -34,14 +34,10 @@ require 'options'
 -- [[ Basic Keymaps ]]
 require 'keymaps'
 
--- [[ Install `lazy.nvim` plugin manager ]]
-require 'lazy-bootstrap'
-
--- [[ Configure and install plugins ]]
-require 'lazy-plugins'
+-- [[ Configure and install plugin manager ]]
+require 'lazy-config'
 
 -- [[ Snippets ]]
 vim.g.tex_flavor = 'latex'
 
--- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
