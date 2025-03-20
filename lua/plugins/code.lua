ee4348f5054dfe488cb7dbfb7bb13deb509908fd
Itron_Al_Lenn <itronallenn@gmail.com>
2025-03-20 22:46:15 +0100

    feat: Conform

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -4,35 +4,23 @@ return {
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
     cmd = { 'ConformInfo' },
+    -- stylua: ignore
     keys = {
-      {
-        '<leader>f',
-        function()
-          require('conform').format { async = true, lsp_fallback = true }
-        end,
-        mode = '',
-        desc = '[F]ormat buffer',
-      },
+      { '<leader>f', function() require('conform').format { async = true, lsp_fallback = true } end, desc = '[F]ormat buffer' },
     },
     opts = {
-      notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = {}
-        return {
-          timeout_ms = 500,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-        }
-      end,
+      format_on_save = { timeout_ms = 500, lsp_format = 'fallback' },
       formatters_by_ft = {
-        html = { 'prettier' },
-        css = { 'prettier' },
+        fish = { 'fish_indent' },
+        html = { 'prettierd' },
+        css = { 'prettierd' },
+        javascript = { 'prettierd' },
         lua = { 'stylua' },
-        python = { 'isort', 'ruff_format' },
+        python = { 'isort', 'ruff' },
         c = { 'clang-format' },
         rust = { 'rustfmt' },
+        bash = { 'shfmt' },
+        json = { 'json-lsp' },
       },
     },
   },
