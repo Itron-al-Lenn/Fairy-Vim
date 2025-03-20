839c3b6875f7714de9a4f1c4cdde7b227922e26d
Itron_Al_Lenn <itronallenn@gmail.com>
2025-03-20 22:48:39 +0100

    feat: Oil

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -18,38 +18,19 @@ return {
   {
     -- oil - allows to edit your file tree like a buffer
     'stevearc/oil.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      CustomOilBar = function()
-        local path = vim.fn.expand '%'
-        path = path:gsub('oil://', '')
-
-        return '  ' .. vim.fn.fnamemodify(path, ':.')
-      end
-
-      require('oil').setup {
-        columns = { 'icon' },
-        keymaps = {
-          ['<C-h>'] = false,
-          ['<C-l>'] = false,
-          ['<C-k>'] = false,
-          ['<C-j>'] = false,
-          ['<M-h>'] = 'actions.select_split',
-        },
-        win_options = {
-          winbar = '%{v:lua.CustomOilBar()}',
-        },
-        view_options = {
-          show_hidden = true,
-        },
-      }
-
-      -- Open parent directory in current window
-      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
-
-      -- Open parent directory in floating window
-      vim.keymap.set('n', '<space>-', require('oil').toggle_float, { desc = 'Open parent directory' })
-    end,
+    lazy = false,
+    dependencies = { 'echasnovski/mini.nvim' },
+    -- stylua: ignore
+    keys = {
+      { '-', function() require('oil').toggle_float() end, desc = 'Open parent directory' },
+    },
+    opts = {
+      columns = { 'icon', 'size' },
+      view_options = { show_hidden = true },
+      keymaps = {
+        ['q'] = { 'actions.close', mode = 'n' },
+      },
+    },
   },
   {
     -- fzf - adds fzf to nvim
