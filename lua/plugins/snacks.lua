f4d5442af5e2a17c261e41862a4d9781c1e675c3
Itron_Al_Lenn <itronallenn@gmail.com>
2025-03-20 22:50:08 +0100

    feat: Snack.Lazygit

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,6 +4,14 @@ return {
     priority = 1000,
     lazy = false,
     ---@type snacks.Config
+    keys = {
+      {
+        '<leader>ol',
+        function()
+          Snacks.lazygit()
+        end,
+        desc = '[O]pen [L]azygit',
+      },
     },
   },
 }
