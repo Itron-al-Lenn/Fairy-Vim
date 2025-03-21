9add3e0574b92b3abefa98a6272fabed6a7e7129
Itron_Al_Lenn <itronallenn@gmail.com>
2025-03-21 12:05:56 +0100

    feat: lang dir

--- lua/plugins/lang/latex.lua
+++ lua/plugins/lang/latex.lua
@@ -1,22 +1,14 @@
 return {
-  {
-    'lervag/vimtex',
-    dependencies = {},
-    event = 'BufReadPre',
-    config = function()
-      vim.cmd 'filetype plugin on'
-      local g = vim.g
+  'lervag/vimtex',
+  dependencies = {},
+  event = 'BufReadPre',
+  config = function()
+    vim.cmd 'filetype plugin on'
+    local g = vim.g
 
-      g.vimtex_quickfix_open_on_warning = false
+    g.vimtex_quickfix_open_on_warning = false
 
-      -- treesitter handles highlights
-      g.vimtex_syntax_enabled = false
-    end,
-  },
-  {
-    'iurimateus/luasnip-latex-snippets.nvim',
-    config = function()
-      require('luasnip.loaders.from_lua').load()
-    end,
-  },
+    -- treesitter handles highlights
+    g.vimtex_syntax_enabled = false
+  end,
 }
