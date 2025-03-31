d147bfd79d5e9403558254e8f041b7fe802d966c
Itron_Al_Lenn <itronallenn@gmail.com>
2025-03-31 14:53:24 +0200

    feat: Snacks.Toggle

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -5,6 +5,7 @@ return {
     lazy = false,
     opts = {
       picker = { enter = true },
+      toggle = { enabled = true },
     },
     -- stylua: ignore
     keys = {
@@ -14,6 +15,23 @@ return {
       { "<leader>on", function() Snacks.picker.notifications(); vim.cmd.stopinsert() end, desc = "Notification History" },
       { '<leader>q',  function() Snacks.picker.diagnostics_buffer(); end, desc = 'Open diagnostic [Q]uickfix list' },
     },
+    init = function()
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'VeryLazy',
+        callback = function()
+          -- Create some toggle mappings
+          Snacks.toggle.option('spell', { name = 'Spelling' }):map '<leader>ts'
+          Snacks.toggle.option('relativenumber', { name = 'Relative Number' }):map '<leader>tL'
+          Snacks.toggle.diagnostics():map '<leader>td'
+          Snacks.toggle.line_number():map '<leader>tl'
+          Snacks.toggle.option('conceallevel', { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map '<leader>tc'
+          Snacks.toggle.treesitter():map '<leader>tT'
+          Snacks.toggle.inlay_hints():map '<leader>th'
+          Snacks.toggle.indent():map '<leader>tg'
+          Snacks.toggle.dim():map '<leader>tD'
+        end,
+      })
+    end,
   },
   {
     'folke/todo-comments.nvim',
