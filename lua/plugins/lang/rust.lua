0f85f2098ac59fe225918064e71931ba12b870a9
Itron_Al_Lenn <itronallenn@gmail.com>
2025-03-31 15:00:27 +0200

    feat: Rust

--- lua/plugins/lang/rust.lua
+++ lua/plugins/lang/rust.lua
@@ -1,17 +1,41 @@
+local util = require 'util.util'
+
 return {
   {
     'saecki/crates.nvim',
     tag = 'stable',
+    ft = { 'rust' },
     event = { 'BufRead Cargo.toml' },
+    keys = function()
+      -- stylua: ignore
+      util.create_autokey({
+        { '<leader>ct', '<cmd>Crates toggle<cr>', { desc = '[T]oggle' } },
+        { '<leader>cr', '<cmd>Crates reload<cr>', { desc = '[R]eload' } },
+        { '<leader>cv', '<cmd>Crates show_versions_popup<cr>', { desc = 'Show [V]ersion Popup' } },
+        { '<leader>cf', '<cmd>Crates show_features_popup<cr>', { desc = 'Show [F]eatures Popup' } },
+        { '<leader>cd', '<cmd>Crates show_dependencies_popup<cr>', { desc = 'Show [D]ependencies Popup' } },
+        { '<leader>cu', '<cmd>Crates update_crate<cr>', { desc = '[U]pdate Crate' } },
+        { '<leader>ca', '<cmd>Crates update_all_crates<cr>', { desc = 'Update [A]ll Crates' } },
+        { '<leader>cU', '<cmd>Crates upgrade_crate<cr>', { desc = '[U]pgrade Crate' } },
+        { '<leader>cA', '<cmd>Crates upgrade_all_crates<cr>', { desc = 'Upgrade [A]ll Crates' } },
+        { '<leader>cH', '<cmd>Crates open_homepage<cr>', { desc = 'Open [H]omepage' } },
+        { '<leader>cR', '<cmd>Crates open_repository<cr>', { desc = 'Open [R]epo' } },
+        { '<leader>cD', '<cmd>Crates open_documentation<cr>', { desc = 'Open [D]ocumentation' } },
+        { '<leader>cC', '<cmd>Crates open_crates_io<cr>', { desc = 'Open [C]rates.io' } },
+        { '<leader>cL', '<cmd>Crates open_lib_rs<cr>', { desc = 'Open [L]ib.rs' } },
+      }, 'BufRead', 'Cargo.toml')
+    end,
     opts = {
-      completion = {
-        cmp = { enabled = true },
+      lsp = {
+        enabled = true,
+        actions = true,
+        completion = true,
+        hover = true,
       },
     },
   },
   {
     'mrcjkb/rustaceanvim',
-    version = vim.fn.has 'nvim-0.10.0' == 0 and '^4' or false,
     ft = { 'rust' },
     opts = {
       server = {
